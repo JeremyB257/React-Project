54e671d8927a58cea08191c2a769affa581dc570
JeremyB257 <bertelootjerem@gmail.com>
2022-09-10 07:55:28 +0200

    fix delete favorite list

--- movie-react-app/src/pages/UserList.js
+++ movie-react-app/src/pages/UserList.js
@@ -1,41 +1,43 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Card from '../components/Card';
 import Header from '../components/Header';
 
 const UserList = () => {
-    const [listData, setListData] = useState([]);
-  
-    useEffect(() => {
-      let moviesId = window.localStorage.movies
-        ? window.localStorage.movies.split(",")
-        : [];
-  
-      for (let i = 0; i < moviesId.length; i++) {
-        axios
-          .get(
-            `https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d&language=fr-FR`
-          )
-          .then((res) => setListData((listData) => [...listData, res.data]));
-      }
-    }, []);
-  
-    return (
-      <div className="user-list-page">
-        <Header />
-        <h2>
-          Coup de coeur <span>💖</span>
-        </h2>
-        <div className="result">
-          {listData.length > 0 ? (
-            listData.map((movie) => <Card movie={movie} key={movie.id} />)
-          ) : (
-            <h2>Aucun coup de coeur pour le moment</h2>
-          )}
-        </div>
+  const [favoriteList, setFavoriteList] = useState([]);
+
+  function getList(moviesId) {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${moviesId}?api_key=ed82f4c18f2964e75117c2dc65e2161d&language=fr-FR`
+    ).then((data) => data.json());
+  }
+  useEffect(() => {
+    let mounted = true;
+    let moviesId = window.localStorage.movies ? window.localStorage.movies.split(',') : [];
+    for (let i = 0; i < moviesId.length; i++) {
+      getList(moviesId[i]).then((items) => {
+        if (mounted) {
+          setFavoriteList((favoriteList) => [...favoriteList, items]);
+        }
+      });
+    }
+    return () => (mounted = false);
+  }, []);
+
+  return (
+    <div className="user-list-page">
+      <Header />
+      <h2>
+        Coup de coeur <span>💖</span>
+      </h2>
+      <div className="result">
+        {favoriteList.length > 0 ? (
+          favoriteList.map((movie) => <Card movie={movie} key={movie.id} />)
+        ) : (
+          <h2>Aucun coup de coeur pour le moment</h2>
+        )}
       </div>
-    );
-  };
-  
-  export default UserList;
-  
+    </div>
+  );
+};
+
+export default UserList;
